d011d4d49dac0f3a88438ce65da3a8b01ce915cd
shehzadkhalid87 <shehzadkhalid04@gmail.com>
2024-07-18 04:29:14 +0500

    new data type file added

---
+++ Data_types/Float.java
@@ -0,0 +1,8 @@
+package Data_types;
+
+public class Float {
+    public static void main(String[] args){
+        float result = (float) ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println(result);
+    }
+}
